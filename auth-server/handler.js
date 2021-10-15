219a1af4fcad6fbac6becb21688431a2260bd83a
IZanko <zankoiva@hotmail.com>
2021-10-15 14:33:06 -0400

    function bug fix

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -103,49 +103,51 @@ module.exports.getAccessToken = async (event) => {
     });
 };
 
-module.exports.getCalendarEvents() = async (event) => {
-  // The values used to instantiate the OAuthClient are at the top of the file
+module.exports.getCalendarEvents = async (event) => {
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-  // Decode authorization code extracted from the URL query
-  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
   oAuth2Client.setCredentials({ access_token });
 
   return new Promise((resolve, reject) => {
-
-    oAuth2Client.getCalendarEvents(access_token, calendar.events.list(
+    calendar.events.list(
       {
         calendarId: calendar_id,
         auth: oAuth2Client,
         timeMin: new Date().toISOString(),
         singleEvents: true,
         orderBy: "startTime",
-      }), (err, response) => {
-        if (err) {
-          return reject(err);
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
         }
-        return resolve(response);
-      });
+      }
+    );
   })
     .then((results) => {
-      // Respond with OAuth token 
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*'
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({ events: results.data.items }),
       };
     })
     .catch((err) => {
-      // Handle error
       console.error(err);
       return {
         statusCode: 500,
-        body: JSON.stringify(response),
-      }
+        body: JSON.stringify(err),
+      };
     });
 };
